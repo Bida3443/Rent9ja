a960971038f574ab8242cdf3f7844fbc2c0a1c01
Bida3443 <alexanderbamise@gmail.com>
2026-05-05 05:22:09 -0700

    Rent9ja

---
+++ app/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Nav () {
+  return (
+    <nav className='bg-[#09200D] min-h-screen h-auto'>
+        <div className='flex justify-between text-white'>
+            <div>ssss</div>
+            <div>fffff</div>
+            <div>ffffff</div>
+        </div>
+    </nav>
+  )
+}
