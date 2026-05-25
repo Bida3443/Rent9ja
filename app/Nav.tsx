01df0b281fe00c7a429771e53b6d040a6c300c90
Bida3443 <alexanderbamise@gmail.com>
2026-05-25 16:45:41 -0700

    Rent9ja

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 export default function Nav () {
   return (
-    <nav className='bg-[#09200D] py-20 px-10  h-auto'>
+    <nav className='bg-[#09200D] py-6 px-10  h-auto'>
         <div className='flex justify-between text-white'>
-            <div>ssss</div>
+            <div><h1 className='text-2xl font-body'><span className='text-[#C9F967]'>Rent</span>9ja</h1></div>
             <div>fffff</div>
             <div>ffffff</div>
-            <div>ffffff</div>
-            <div>ffffff</div>
-            <div>ffffff</div>
-            <div>ffffff</div>
+            
             
             
         </div>
