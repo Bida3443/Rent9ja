e03cf2c151983b4804050d223363f1f91d7cb65d
Bida3443 <alexanderbamise@gmail.com>
2026-05-26 15:30:12 -0700

    Rent9ja

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -21,18 +21,23 @@ export default function Nav() {
    
   return (
 
-    <nav className='bg-[#09200D] py-6 px-10  h-auto'>
+    <nav className='bg-[#09200D] py-6 px-10 sticky top-0 h-auto'>
       <div className='flex justify-between items-center text-white'>
         
-        <a href="/"><h1 className='text-2xl font-body'><span className='text-[#C9F967]'>Rent</span>9ja</h1></a>
-        <div className='flex gap-5 text-[#B4B8AB]'>
+        <a href="/"><h1 className='text-2xl font-extrabold tracking-tight'><span className='text-[#C9F967]'>Rent</span>9ja</h1></a>
+        <div className='flex gap-5 text-[#B4B8AB] '>
           {Nav.map((item,index)=>(
 
-            <div key={index}></div>
+            <div key={index} className='hover:text-white transition-all lg:block hidden duration-300'>
+              
+              <a href={item.link}>
+                {item.text}
+              </a >
+            </div>
           ))}
         </div>
         
-        <a href='' className='bg-[#C9F967] py-2 px-4 text-[14px] rounded-full text-black transition-all duration-300 transform ease-in-out cursor-pointer'>List your Property</a>
+        <a href='' className='bg-[#C9F967] py-2 px-4 text-[14px] rounded-full text-black transform transition-transform hover:-translate-y-0.5 duration-300 ease-in-out'>List your Property</a>
       </div>
     </nav>
   )
