c44e11215c6239466660b929b857040547bc7ae4
Bida3443 <alexanderbamise@gmail.com>
2026-05-30 11:31:58 -0700

    Rent9ja

--- app/Work.tsx
+++ app/Work.tsx
@@ -1,7 +1,7 @@
 export default function Work() {
     return (
         <section id="Work" className="bg-[#09200D] w-full min-h-screen">
-            <div className="flex py-25 px-10 flex-col">
+            <div className="flex py-25 lg:px-10 px-5 flex-col">
                 <div className="space-y-5">
                     <p className="text-[#C9F967] text-[13px] font-extrabold tracking-tight">How it works</p>
                     <h1 className="text-[#EFECE2] font-extrabold tracking-tight leading-[1.07] text-4xl sm:text-6xl md:text-7xl lg:text-[60px]">Direct. Simple. Yours.</h1>
